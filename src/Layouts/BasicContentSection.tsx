f9dff1f9b674466545b517c47cc46fdefe9b3de3
fubira <fubira@gmail.com>
2020-12-18 02:32:55 +0900

    BasicContentSection

---
+++ src/Layouts/BasicContentSection.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { breakpointDown } from '@paljs/ui/breakpoints';
+
+const ContentSectionStyle = styled.div`
+  display: flex;
+  max-width: 1020px;
+  margin: 0 auto;
+`;
+
+const SideMenuStyle = styled.div`
+  ${breakpointDown('sm')`
+    display: none;
+  `}
+`;
+
+const ContentSizeStyle = styled.div`
+  min-width: 320px;
+  width: 100%;
+`;
+
+interface ContentSectionProps {
+  className?: string;
+  sidemenu?: React.ReactNode;
+}
+
+const BasicContentSection: React.FC<ContentSectionProps> = ({ children, className, sidemenu }) => {
+  return (
+    <ContentSectionStyle className={className}>
+      {sidemenu && <SideMenuStyle>{sidemenu}</SideMenuStyle>}
+      <ContentSizeStyle> {children} </ContentSizeStyle>
+    </ContentSectionStyle>
+  );
+};
+
+export default BasicContentSection;
--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -2,28 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 import PageLayout from 'layouts/PageLayout';
+import BasicContentSection from 'layouts/BasicContentSection';
 import TextPart from 'components/TextPart';
-import InfoPageMenu from '../components/InfoPageMenu/index';
+import InfoPageMenu from 'components/InfoPageMenu';
 import { breakpointDown } from '@paljs/ui/breakpoints';
 
-const FlexContentStyle = styled.div`
-  display: flex;
-  max-width: 1020px;
-  margin: 1rem auto;
-
-  ${breakpointDown('sm')`
-    .side-menu {
-      display: none;
-    }
-  `}
-`;
-
-const ContentSizeStyle = styled.div`
-  min-width: 320px;
-  width: 100%;
-`;
-
-const MainContentStyle = styled.div`
+const TableStyle = styled.div`
   margin: auto;
   padding: 2rem;
   min-width: 320px;
@@ -62,136 +46,65 @@ const MainContentStyle = styled.div`
   }
 `;
 
-export default function Index() {
+export default function Company() {
   return (
     <PageLayout title="Landing">
-      <FlexContentStyle>
-        <InfoPageMenu className="side-menu" />
+      <BasicContentSection sidemenu={<InfoPageMenu />}>
+        <TableStyle>
+          <h2>運営会社</h2>
+          <TextPart className="text-part">
+            <dl>
+              <div>
+                <dt> 運営会社 </dt>
+                <dd> 株式会社 仮 </dd>
+              </div>
 
-        <ContentSizeStyle>
-          <MainContentStyle>
-            <h2>運営会社</h2>
-            <TextPart className="text-part">
-              <dl>
-                <div>
-                  <dt> 運営会社 </dt>
-                  <dd> 株式会社 仮 </dd>
-                </div>
+              <div>
+                <dt> 設立 </dt>
+                <dd> 2020年12月15日 </dd>
+              </div>
 
-                <div>
-                  <dt> 設立 </dt>
-                  <dd> 2020年12月15日 </dd>
-                </div>
+              <div>
+                <dt> 資本金 </dt>
+                <dd> 1,234万円 </dd>
+              </div>
 
-                <div>
-                  <dt> 資本金 </dt>
-                  <dd> 1,234万円 </dd>
-                </div>
+              <div>
+                <dt> 所在地 </dt>
+                <dd>
+                  <p>郵便番号123-4567</p>
+                  <p>東京都千代田区1-1</p>
+                </dd>
+              </div>
 
-                <div>
-                  <dt> 所在地 </dt>
-                  <dd>
-                    <p>郵便番号123-4567</p>
-                    <p>東京都千代田区1-1</p>
-                  </dd>
-                </div>
+              <div>
+                <dt> 事業内容 </dt>
+                <dd>
+                  <ul>
+                    <li>ソフトウェア開発</li>
+                    <li>ソフトウェア開発</li>
+                    <li>ソフトウェア開発</li>
+                  </ul>
+                </dd>
+              </div>
 
-                <div>
-                  <dt> 事業内容 </dt>
-                  <dd>
-                    <ul>
-                      <li>ソフトウェア開発</li>
-                      <li>ソフトウェア開発</li>
-                      <li>ソフトウェア開発</li>
-                    </ul>
-                  </dd>
-                </div>
-
-                <div>
-                  <dt> 役員 </dt>
-                  <dd>
-                    <p>代表取締役社長</p>
-                    <p>取締役</p>
-                    <p>取締役</p>
-                  </dd>
-                  <dd>
-                    <p>鈴木 二郎</p>
-                    <p>田中 将太</p>
-                    <p>西出 輝裕</p>
-                  </dd>
-                </div>
-              </dl>
-            </TextPart>
-          </MainContentStyle>
-        </ContentSizeStyle>
-      </FlexContentStyle>
+              <div>
+                <dt> 役員 </dt>
+                <dd>
+                  <p>代表取締役社長</p>
+                  <p>取締役</p>
+                  <p>取締役</p>
+                </dd>
+                <dd>
+                  <p>鈴木 二郎</p>
+                  <p>田中 将太</p>
+                  <p>西出 輝裕</p>
+                </dd>
+              </div>
+            </dl>
+          </TextPart>
+        </TableStyle>
+      </BasicContentSection>
     </PageLayout>
   );
 }
-
-/*
-      <FlexContentStyle>
-        <SideMenuStyle>
-          <MenuSection>
-            <Menu nextJs ref={menuRef} Link={Link} currentPath={router.pathname} items={leftMenuItems} />
-          </MenuSection>
-        </SideMenuStyle>
-
-        <ContentStyle>
-          <ContentSection>
-            <h2>運営会社</h2>
-            <Part>
-              <dl>
-                <div>
-                  <dt> 運営会社 </dt>
-                  <dd> 株式会社 仮 </dd>
-                </div>
-
-                <div>
-                  <dt> 設立 </dt>
-                  <dd> 2020年12月15日 </dd>
-                </div>
-
-                <div>
-                  <dt> 資本金 </dt>
-                  <dd> 1,234万円 </dd>
-                </div>
-
-                <div>
-                  <dt> 所在地 </dt>
-                  <dd>
-                    <p>郵便番号123-4567</p>
-                    <p>東京都千代田区1-1</p>
-                  </dd>
-                </div>
-
-                <div>
-                  <dt> 事業内容 </dt>
-                  <dd>
-                    <ul>
-                      <li>ソフトウェア開発</li>
-                      <li>ソフトウェア開発</li>
-                      <li>ソフトウェア開発</li>
-                    </ul>
-                  </dd>
-                </div>
-
-                <div>
-                  <dt> 役員 </dt>
-                  <dd>
-                    <p>代表取締役社長</p>
-                    <p>取締役</p>
-                    <p>取締役</p>
-                  </dd>
-                  <dd>
-                    <p>鈴木 二郎</p>
-                    <p>田中 将太</p>
-                    <p>西出 輝裕</p>
-                  </dd>
-                </div>
-              </dl>
-            </Part>
-          </ContentSection>
-        </ContentStyle>
-      </FlexContentStyle>
-*/
